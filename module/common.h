6cc1df4baaf78cec801e1daac3530c93ab30c960
SleekZ <40346672+SleekZ@users.noreply.github.com>
2025-01-15 17:00:50 +1100

    Update common.h
    - Add algorithm header to fix 'sort': is not a member of 'std'

--- module/common.h
+++ module/common.h
@@ -6,6 +6,7 @@
 #include <mutex>
 #include <optional>
 #include <vector>
+#include <algorithm>
 
 #define LOG_INFO(fmt, ...) printf("[+] " fmt "\n", ##__VA_ARGS__)
 #define LOG_ERROR(fmt, ...) printf("[-] " fmt "\n", ##__VA_ARGS__)
